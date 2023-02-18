8248ec1d94a560c700bfa54228e40fa49679080f
Bogdan Lazar <71758173+BogdanLazar01@users.noreply.github.com>
2023-02-18 15:08:37 +0100

    Now with a back-end

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,11 +1,12 @@
-import Link from "next/link";
-import classes from "./button.module.css";
+import Link from 'next/link';
+
+import classes from './button.module.css';
 
 function Button(props) {
   if (props.link) {
     return (
-      <Link href={props.link} className={classes.btn}>
-        {props.children}
+      <Link href={props.link}>
+        <a className={classes.btn}>{props.children}</a>
       </Link>
     );
   }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,25 +1,48 @@
-import { getAllEvents } from "../../dummy-data";
-import EventList from "../../components/events/event-list";
-import EventsSearch from "../../components/events/events-search";
-import { Fragment } from "react";
-import { useRouter } from "next/router";
+import { Fragment } from 'react';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 
-function EventsPage() {
+import { getAllEvents } from '../../helpers/api-util';
+import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
+
+function AllEventsPage(props) {
   const router = useRouter();
-  const events = getAllEvents();
+  const { events } = props;
 
   function findEventsHandler(year, month) {
-    const fullPath = `/events/${year}/${month}`
+    const fullPath = `/events/${year}/${month}`;
 
     router.push(fullPath);
   }
 
   return (
     <Fragment>
+      <Head>
+        <title>All my events</title>
+      </Head>
+      <Head>
+        <title>All Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
+      </Head>
       <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
     </Fragment>
   );
 }
 
-export default EventsPage;
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60
+  };
+}
+
+export default AllEventsPage;
